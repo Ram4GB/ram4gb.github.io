7dce1661b176b590add6bf1553f160712d4c10a3
Ram4GB <leminhcuong2988@yahoo.com.vn>
2020-02-14 16:55:29 +0700

    allow using config public

---
+++ Quote_Firebase/config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: 'AIzaSyDdEbbk0k_sPxoXEEpSbzwpwZj_vrLUdSQ',
+  authDomain: 'todo-e5614.firebaseapp.com',
+  databaseURL: 'https://todo-e5614.firebaseio.com',
+  projectId: 'todo-e5614',
+  storageBucket: 'todo-e5614.appspot.com',
+  messagingSenderId: '231486909630',
+  appId: '1:231486909630:web:51e53168751688a29f42ef'
+}
+
+firebase.initializeApp(firebaseConfig)
